7b6fa8062de5ce1bc0a1b0623071f4163191c392
Arif Fathur Mahmuda <afmahmuda@live.com>
2016-09-12 16:23:11 +0700

    redefine rotation matrix for multi axis rotation, the order is x,y,z

--- RayTracer/Transform/Rotation.cs
+++ RayTracer/Transform/Rotation.cs
@@ -21,25 +21,29 @@ namespace RayTracer.Transformation
         {
             Matrix = new MyMatrix(4, 4);
 
-            Double rad = (angle) * Math.PI / 180;
+            Double rad = (angle) * Math.PI / 180.0;
 
-            Double x = values.X;
-            Double y = values.Y;
-            Double z = values.Z;
-            Double c = Math.Cos(rad);
-            Double s = Math.Sin(rad);
+            Double x = values.X * rad;
+            double cx = Math.Cos(x);
+            double sx = Math.Sin(x);
+            Double y = values.Y * rad;
+            double cy = Math.Cos(y);
+            double sy = Math.Sin(y);
+            Double z = values.Z * rad;
+            double cz = Math.Cos(z);
+            double sz = Math.Sin(z);
 
-            Matrix.SetValue(0, 0, x * x * (1 - c) + c);
-            Matrix.SetValue(0, 1, x * y * (1 - c) - z * s);
-            Matrix.SetValue(0, 2, x * z * (1 - c) + y * s);
+            Matrix.SetValue(0, 0, cy *cz);
+            Matrix.SetValue(0, 1, cz*sx*sy-cx*sz);
+            Matrix.SetValue(0, 2, cx*cz*sy+sx*sz);
 
-            Matrix.SetValue(1, 0, y * x * (1 - c) + z * s);
-            Matrix.SetValue(1, 1, y * y * (1 - c) + c);
-            Matrix.SetValue(1, 2, y * z * (1 - c) - x * s);
+            Matrix.SetValue(1, 0, cy*sz);
+            Matrix.SetValue(1, 1, cx*cz+sx*sy*sz);
+            Matrix.SetValue(1, 2, -cz*sx+cx*sy*sz);
 
-            Matrix.SetValue(2, 0, z * x * (1 - c) - y * s);
-            Matrix.SetValue(2, 1, z * y * (1 - c) + x * s);
-            Matrix.SetValue(2, 2, z * z * (1 - c) + c);
+            Matrix.SetValue(2, 0, -sy);
+            Matrix.SetValue(2, 1, cy*sx);
+            Matrix.SetValue(2, 2, cx*cy);
 
             Matrix.SetValue(3, 3, 1f);
         }
