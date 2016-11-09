87e561f4689e60e3e89c36aa5350e41ca7113d5b
afmahmuda <afmahmuda@live.com>
2016-11-09 16:36:18 +0700

    edit "epsilon" value

--- RayTracer - BVH - CUDA/RayTracerCUDA/Ray.h
+++ RayTracer - BVH - CUDA/RayTracerCUDA/Ray.h
@@ -19,8 +19,8 @@ public:
 	float intersectDist = FLT_MAX;
 	std::shared_ptr< Triangle > intersectWith;
 	Vec3 getHitReal() const { return Vec3(start + Vec3(direction * intersectDist)); }
-	Vec3 getHitPlus() const { return Vec3(start + Vec3(direction * (intersectDist * 1.00001f))); }
-	Vec3 getHitMin() const { return Vec3(start + Vec3(direction * (intersectDist * 0.99999f))); }
+	Vec3 getHitPlus() const { return Vec3(start + Vec3(direction * (intersectDist * 1.0001f))); }
+	Vec3 getHitMin() const { return Vec3(start + Vec3(direction * (intersectDist * 0.9999f))); }
 
 
 	bool isCloser(float dist);
