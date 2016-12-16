1bbc8f5644260b22fb04fc0a2ce87f8faf3cb3ef
Arif Fathur Mahmuda <afmahmuda@live.com>
2016-12-16 18:27:16 +0700

    reformat for docs

--- RayTracer - CPP - BVH - OBJ/RayTracer/Program.cpp
+++ RayTracer - CPP - BVH - OBJ/RayTracer/Program.cpp
@@ -1,4 +1,4 @@
-#include <fstream>	//file 
+#include <fstream>  //file 
 #include <iostream>
 #include <algorithm> //replace
 #include <string>
@@ -11,37 +11,37 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-	//scene file name, default = "default.scene"	
-	string filename = (argc >= 2) ? argv[1] : "scene_default.scene";
-	std::replace(filename.begin(), filename.end(), '\\', '/');
+  //scene file name, default = "default.scene"  
+  string filename = (argc >= 2) ? argv[1] : "scene_default.scene";
+  std::replace(filename.begin(), filename.end(), '\\', '/');
 
-	//using aac algorithm (1 = yes / 0 = no ), default = yes;
-	bool isAAC = (argc >= 3) ? (atoi(argv[2]) == 1) ? true : false : true;
+  //using aac algorithm (1 = yes / 0 = no ), default = yes;
+  bool isAAC = (argc >= 3) ? (atoi(argv[2]) == 1) ? true : false : true;
 
-	//bin type (b = box / s = sphere ), default = box;
-	Container::TYPE binType = (argc >= 4) ? (argv[3][0] == 'b') ? Container::BOX : Container::SPHERE : Container::BOX;
+  //bin type (b = box / s = sphere ), default = box;
+  Container::TYPE binType = (argc >= 4) ? (argv[3][0] == 'b') ? Container::BOX : Container::SPHERE : Container::BOX;
 
-	//aac treshold, def=20;
-	int thres = (argc >= 5) ? (atoi(argv[4])) : 20;
+  //aac treshold, def=20;
+  int thres = (argc >= 5) ? (atoi(argv[4])) : 20;
 
 
-	//thread number for aac and 
-	(argc >= 6) ? ThreadPool::setMaxThread((atoi(argv[5]) - 1)) : ThreadPool::setMaxThread(std::thread::hardware_concurrency() - 1);
+  //thread number for aac and 
+  (argc >= 6) ? ThreadPool::setMaxThread((atoi(argv[5]) - 1)) : ThreadPool::setMaxThread(std::thread::hardware_concurrency() - 1);
 
-	//thread number for tracing
-	int traceTN = 8 * std::thread::hardware_concurrency();
+  //thread number for tracing
+  int traceTN = 8 * std::thread::hardware_concurrency();
 
-	ifstream myfile(filename);
-	if (myfile.is_open())
-	{
-		myfile.close();
-		TraceManager tracer(traceTN, binType, isAAC, thres);
-		tracer.traceScene(filename);
-	}
-	else
-	{
-		cout << filename << " file not found!" << endl;
-	}
-	//	system("pause");
-	return 0;
+  ifstream myfile(filename);
+  if (myfile.is_open())
+  {
+    myfile.close();
+    TraceManager tracer(traceTN, binType, isAAC, thres);
+    tracer.traceScene(filename);
+  }
+  else
+  {
+    cout << filename << " file not found!" << endl;
+  }
+  //  system("pause");
+  return 0;
 }
--- RayTracer - CPP - BVH - OBJ/RayTracer/RayManager.cpp
+++ RayTracer - CPP - BVH - OBJ/RayTracer/RayManager.cpp
@@ -7,148 +7,148 @@ RayManager::RayManager()
 
 void RayManager::traceRay(Ray & ray, std::shared_ptr<Container> bvh)
 {
-	std::vector<std::shared_ptr<Container>> bins;
-	bins.push_back(bvh);
-	while (bins.size() > 0)
-	{
-		std::shared_ptr<Container> currBin = bins.back();
-		bins.pop_back(); 
-		ray.hitCount[0]++;
-		if (currBin->isIntersecting(ray))
-		{
-			if (currBin->geo != nullptr)
-			{
-				ray.hitCount[1]++;
-				if (currBin->geo->isIntersecting(ray))
-					ray.intersectWith = currBin->geo;
-			}
-			else
-			{
-				bins.push_back(currBin->rChild);
-				bins.push_back(currBin->lChild);
-			}
-		}
-	}
+ std::vector<std::shared_ptr<Container>> bins;
+ bins.push_back(bvh);
+ while (bins.size() > 0)
+ {
+  std::shared_ptr<Container> currBin = bins.back();
+  bins.pop_back(); 
+  ray.hitCount[0]++;
+  if (currBin->isIntersecting(ray))
+  {
+   if (currBin->geo != nullptr)
+   {
+    ray.hitCount[1]++;
+    if (currBin->geo->isIntersecting(ray))
+     ray.intersectWith = currBin->geo;
+   }
+   else
+   {
+    bins.push_back(currBin->rChild);
+    bins.push_back(currBin->lChild);
+   }
+  }
+ }
 }
 
 MyColor RayManager::getColor(const Ray & ray, Scene & scene, int bounce) {
-	if (bounce <= 0 || ray.intersectWith == nullptr)
-	{
-		return MyColor();
-		//return scene.defColor;
-	}
-	else
-	{
-		std::vector<std::shared_ptr< Light>> effectiveLights = populateLights(ray, scene.lights, scene.bin);
-		MyColor color =
-			ray.intersectWith->mat.refrVal *((MyColor(1, 1, 1) - ray.intersectWith->mat.reflVal)* calcColor(ray, effectiveLights, scene.getAtt()) +
-				ray.intersectWith->mat.reflVal * getRefl(ray, scene, bounce - 1)) +
-			(1 - ray.intersectWith->mat.refrVal) * getRefr(ray, scene, bounce - 1);
-		return color;
-	}
+ if (bounce <= 0 || ray.intersectWith == nullptr)
+ {
+  return MyColor();
+  //return scene.defColor;
+ }
+ else
+ {
+  std::vector<std::shared_ptr< Light>> effectiveLights = populateLights(ray, scene.lights, scene.bin);
+  MyColor color =
+   ray.intersectWith->mat.refrVal *((MyColor(1, 1, 1) - ray.intersectWith->mat.reflVal)* calcColor(ray, effectiveLights, scene.getAtt()) +
+   ray.intersectWith->mat.reflVal * getRefl(ray, scene, bounce - 1)) +
+   (1 - ray.intersectWith->mat.refrVal) * getRefr(ray, scene, bounce - 1);
+  return color;
+ }
 }
 
 MyColor RayManager::getRefl(const Ray & ray, Scene & scene, int bounce) {
-	float cosI = ray.intersectWith->getNormal(ray.getHit()) * ray.direction;
-	Vec3 normal = ray.intersectWith->getNormal(ray.getHit());
-	if (cosI < 0)
-	{
-		normal *= -1.f;
-	}
-	Vec3 newDir = ray.direction + (normal * 2.0f * -(normal * ray.direction));
-	Ray reflectRay(ray.getHitMin(), newDir);
-	reflectRay.type = Ray::REFLECTION;
-	traceRay(reflectRay, scene.bin);
-	return getColor(reflectRay, scene, bounce);
+ float cosI = ray.intersectWith->getNormal(ray.getHit()) * ray.direction;
+ Vec3 normal = ray.intersectWith->getNormal(ray.getHit());
+ if (cosI < 0)
+ {
+  normal *= -1.f;
+ }
+ Vec3 newDir = ray.direction + (normal * 2.0f * -(normal * ray.direction));
+ Ray reflectRay(ray.getHitMin(), newDir);
+ reflectRay.type = Ray::REFLECTION;
+ traceRay(reflectRay, scene.bin);
+ return getColor(reflectRay, scene, bounce);
 }
 
 MyColor RayManager::getRefr(const Ray & ray, Scene & scene, int bounce) {
-	if (ray.intersectWith->mat.refrVal != 1.0)
-	{
-		float cosI = ray.intersectWith->getNormal(ray.getHit()) * ray.direction;
-		Vec3 normal;
-		float n1, n2, n;
-		normal = ray.intersectWith->getNormal(ray.getHit());
-		if (cosI > 0)
-		{
-			n1 = ray.intersectWith->mat.refractIndex;
-			n2 = 1;
-			normal *= -1.0f;
-		}
-		else
-		{
-			n1 = 1;
-			n2 = ray.intersectWith->mat.refractIndex;
-			cosI = -cosI;
-		}
-		n = n1 / n2;
-		float sinT2 = n * n * (1.0f - cosI * cosI);
-		float cosT2 = (1.0f - sinT2);
-		float cosT = (float)sqrtf(1.0f - sinT2);
+ if (ray.intersectWith->mat.refrVal != 1.0)
+ {
+  float cosI = ray.intersectWith->getNormal(ray.getHit()) * ray.direction;
+  Vec3 normal;
+  float n1, n2, n;
+  normal = ray.intersectWith->getNormal(ray.getHit());
+  if (cosI > 0)
+  {
+   n1 = ray.intersectWith->mat.refractIndex;
+   n2 = 1;
+   normal *= -1.0f;
+  }
+  else
+  {
+   n1 = 1;
+   n2 = ray.intersectWith->mat.refractIndex;
+   cosI = -cosI;
+  }
+  n = n1 / n2;
+  float sinT2 = n * n * (1.0f - cosI * cosI);
+  float cosT2 = (1.0f - sinT2);
+  float cosT = (float)sqrtf(1.0f - sinT2);
 
 
 
-		float rn = (n1 * cosI - n2 * cosT) / (n1 * cosI + n2 * cosT);
-		float rt = (n2 * cosI - n1 * cosT) / (n2 * cosI + n2 * cosT);
-		rn *= rn;
-		rt *= rt;
-		float refl = (rn + rt) * .5f;
-		float trans = 1.0f - refl;
+  float rn = (n1 * cosI - n2 * cosT) / (n1 * cosI + n2 * cosT);
+  float rt = (n2 * cosI - n1 * cosT) / (n2 * cosI + n2 * cosT);
+  rn *= rn;
+  rt *= rt;
+  float refl = (rn + rt) * .5f;
+  float trans = 1.0f - refl;
 
-		if (cosT2 < 0)
-		{
-			//if (bounce - 1 == 0)
-			//    return scene.defColor;
-			return ray.intersectWith->mat.reflVal *getRefl(ray, scene, bounce - 1);
-		}
+  if (cosT2 < 0)
+  {
+   //if (bounce - 1 == 0)
+   //    return scene.defColor;
+   return ray.intersectWith->mat.reflVal *getRefl(ray, scene, bounce - 1);
+  }
 
-		Vec3 newDir = ray.direction * n + normal * (n * cosI - cosT);
-		Ray refractRay(ray.getHitPlus(), newDir);
-		refractRay.type = Ray::REFRACTION;
-		traceRay(refractRay, scene.bin);
-		return (getColor(refractRay, scene, bounce));
-	}
-	else return MyColor();
+  Vec3 newDir = ray.direction * n + normal * (n * cosI - cosT);
+  Ray refractRay(ray.getHitPlus(), newDir);
+  refractRay.type = Ray::REFRACTION;
+  traceRay(refractRay, scene.bin);
+  return (getColor(refractRay, scene, bounce));
+ }
+ else return MyColor();
 }
 
 MyColor RayManager::calcColor(const Ray & ray, std::vector<std::shared_ptr<Light>> effectiveLights, Attenuation & attenuation)
 {
-	MyColor result = ray.intersectWith->mat.ambient + ray.intersectWith->mat.emmission;
-	Vec3 normal = ray.intersectWith->getNormal(ray.getHitMin());
-	float cosI = ray.intersectWith->getNormal(ray.getHit()) * ray.direction;
-	if (cosI > 0)
-	{
-		normal *= -1.f;
-	}
+ MyColor result = ray.intersectWith->mat.ambient + ray.intersectWith->mat.emmission;
+ Vec3 normal = ray.intersectWith->getNormal(ray.getHitMin());
+ float cosI = ray.intersectWith->getNormal(ray.getHit()) * ray.direction;
+ if (cosI > 0)
+ {
+  normal *= -1.f;
+ }
 
-	for (int i = 0; i < effectiveLights.size(); i++)
-	{
-		std::shared_ptr< Light> light = effectiveLights[i];
-		Vec3 pointToLight = light->getPointToLight(ray.getHitMin());
-		Vec3 halfAngleToLight = ((ray.direction * -1.0f) + pointToLight);
-		halfAngleToLight = halfAngleToLight.normalize();
+ for (int i = 0; i < effectiveLights.size(); i++)
+ {
+  std::shared_ptr< Light> light = effectiveLights[i];
+  Vec3 pointToLight = light->getPointToLight(ray.getHitMin());
+  Vec3 halfAngleToLight = ((ray.direction * -1.0f) + pointToLight);
+  halfAngleToLight = halfAngleToLight.normalize();
 
-		Material material = ray.intersectWith->mat;
+  Material material = ray.intersectWith->mat;
 
-		float attenuationValue = light->getAttValue(ray.getHitMin(), attenuation);
-		pointToLight = pointToLight.normalize();
-		result +=
-			attenuationValue * (*(light->color)) *
-			((material.diffuse * (pointToLight * normal)) +
-				(material.specular * std::powf(halfAngleToLight * normal, material.shininess)));
-	}
-	return result;
+  float attenuationValue = light->getAttValue(ray.getHitMin(), attenuation);
+  pointToLight = pointToLight.normalize();
+  result +=
+   attenuationValue * (*(light->color)) *
+   ((material.diffuse * (pointToLight * normal)) +
+    (material.specular * std::powf(halfAngleToLight * normal, material.shininess)));
+ }
+ return result;
 }
 
 std::vector<std::shared_ptr<Light>> RayManager::populateLights(const Ray & ray, std::vector<std::shared_ptr<Light>> allLights, std::shared_ptr<Container> bvh)
 {
-	std::vector <std::shared_ptr<Light>> res;
-	for (size_t i = 0; i < allLights.size(); i++)
-	{
-		if (allLights[i]->isEffective(ray.getHitMin(), bvh))
-			res.push_back(allLights[i]);
-	}
-	return res;
+ std::vector <std::shared_ptr<Light>> res;
+ for (size_t i = 0; i < allLights.size(); i++)
+ {
+  if (allLights[i]->isEffective(ray.getHitMin(), bvh))
+   res.push_back(allLights[i]);
+ }
+ return res;
 }
 
 RayManager::~RayManager()
